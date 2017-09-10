34bc0337b33fc503fc1d3b283a614f93d4e869c1
Sanjay Prajapati <srprajapati245@gmail.com>
2017-09-10 14:19:52 +0530

    Set welcome message in main method

--- src/com/kavinaam/Main.java
+++ src/com/kavinaam/Main.java
@@ -3,6 +3,7 @@ package com.kavinaam;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code
+        System.out.println("Welcome to git tutorial");
     }
 }
